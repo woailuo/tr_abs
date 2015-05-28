2cfa0e95c533769da3d982380ef69716906b5095
woailuo <ki.tan@hotmail.com>
2015-05-28 12:11:47 +0900

    update

---
+++ test/t1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void f(void (*)(unsigned int));
+int func(int);
+
+
+int main()
+{
+  int *q = (int*)malloc(sizeof(int));
+  int *r = (int*)malloc(sizeof(int));
+  int c =0;
+  scanf("%d",&c);
+
+  if(q)
+    {
+
+      if(c>1) // raiseNullExExpr (BinOp(..)) (...)
+        {
+          /* q = r; */
+          /* printf("%d\n", conn->num); */
+          /* f(free); */
+          printf("%d\n",*q + 1);
+          /* q =(int *)malloc(sizeof(int)); */
+          /* q = func(*q); */
+          /* int m = *q; */
+          /* printf("%x\n",*q); */
+          int m = *q;
+        }
+
+      free(q);
+
+    }
+
+  /*
+    int m = 1;
+    if (c > 1) {
+      q = r;
+      int m = *q;
+     }
+     free(q)
+   */
+
+  return 0;
+}
