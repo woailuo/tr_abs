4c35d55e44be4db905985f56f71d9872d9fb4011
woailuo <ki.tan@hotmail.com>
2015-06-11 01:51:40 +0900

    update abs.ml: printing

--- abs.ml
+++ abs.ml
@@ -5,37 +5,56 @@ module E = Errormsg
 let str = ref ""
 let funclist = ref (("","") :: [])
 
-(* let rec fixLval (lv: lval): string = *)
-(*   match lv with *)
-(*       (Var a, NoOffset) when (a.vname = "malloc" || a.vname = "free"）->  a.vname *)
-(*     | (Mem exp, _) ->  *)
-(*     | (Var a,_)  -> ( *)
-(*       try *)
-(*         (List.assoc a.vname funclist.contents) ^ ";" *)
-(*       with *)
-(*           _ -> "" *)
-(*     ) *)
-(*     | _ -> "" *)
+let rec fixLval (fname:string) (lv: lval): string =
+  match lv with
+  | (Var a, _)  ->
+     (
+       try
+         (List.assoc a.vname funclist.contents)
+       with
+         _ -> ""
+     )
+  | _ -> ""
 
-(* and fixCallExpr (expr: exp): string = *)
+(* and findCall (fname : string) (expr: exp): string = *)
 (*   match expr with *)
-(*     | Lval a -> fixLval  a *)
+(*     | Lval a -> fixLval fname a *)
 (*     | Const _ *)
 (*     | SizeOf _  | SizeOfE _ | SizeOfStr _  | AlignOf _ *)
 (*     | AlignOfE _ | UnOp _  | BinOp  _  | Question _ *)
 (*     | CastE _  | AddrOf _  | AddrOfLabel _  | StartOf _ -> "" *)
 
-let rec  fixinstrs (ins:instr list) : string =
+and fixExpr (fname : string) (expr: exp) : string =
+  match expr with
+  | Lval a -> fixLval fname a
+  | UnOp (unop, e1, typ) -> fixExpr fname e1
+  | BinOp (binop, e1,e2, typ) -> let str1 = fixExpr  fname e1 in
+                                 let str2 = fixExpr fname e2 in
+                                 str1 ^ str2
+  | Question (e1,e2,e3,typ) -> let str1 = fixExpr  fname e1 in
+                               let str2 = fixExpr fname e2 in
+                               let str3 = fixExpr fname e3 in
+                                 str1 ^ str2 ^ str3
+  | CastE (typ, e) -> let str1 = fixExpr fname e in str1
+  | Const _
+  | SizeOf _  | SizeOfE _ | SizeOfStr _  | AlignOf _
+  | AlignOfE _
+  | AddrOf _  | AddrOfLabel _  | StartOf _ -> ""
+
+and fixinstrs (fname : string) (ins:instr list) : string =
   match ins with
-    | i :: rest-> let s,b = fixInstr i in
-                   if b then (s^";" ^ (fixinstrs rest)) else (fixinstrs rest)
+  | i :: rest-> let s,b = fixInstr fname i in
+                  if b then(s ^";" ^ (fixinstrs fname rest)) else (fixinstrs fname rest)
     | [] -> ""
 
-and fixInstr (i : instr): string* bool  =
+and fixInstr (fname : string) (i : instr): string* bool  =
   match i with
   | Set _  -> "", false
-  | Call (_, (Lval (Var a, NoOffset)), exprs ,location) when (a.vname = "free" || a.vname = "malloc")   ->
-         a.vname, true  (* fixCallExpr expr *)(* todo : exprs *)
+  | Call (_, (Lval (Var a, NoOffset)), exprs ,location) when (a.vname = "free" || a.vname = "malloc")   ->  a.vname, true  (* todo : exprs *)
+  | Call (_,((Lval (Var a, _)) as e1), exps, loc) ->
+     let str1 = fixExpr fname e1 in
+     if a.vname = fname then ("a",true) else
+       (if (String.length str1) > 0 then (str1,true) else ("",false))
   | Call _ -> "",false
   | Asm _-> "",false
 
@@ -62,16 +81,22 @@ and isContainsSemi (str: string) : bool =
 
 and deleteSemi (str: string) : string =
   let length = String.length str in
-  if (length > 0) then (String.sub str 0 (length-1)) else ""
+  if (length > 0) then
+    (
+      let substr = String.sub str (length-1) 1 in
+      if substr = ";" then String.sub str 0 (length-1)
+      else str
+    )
+  else ""
 
-and fixStmt (s : stmt) : string =
+and fixStmt (fname : string) (s : stmt) : string =
   match s.skind with
   | Instr il ->
-    let str = fixinstrs il in
+    let str = fixinstrs fname il in
     deleteSemi str
   | If(_,tb,fb,_) ->
-     let sb1 = fixBlock tb in
-     let sb2 = fixBlock fb in
+     let sb1 = fixBlock fname tb in
+     let sb2 = fixBlock fname fb in
      let b1 = findMorF sb1 in
      let b2 = findMorF sb2 in
      begin
@@ -80,8 +105,6 @@ and fixStmt (s : stmt) : string =
          (
            let b3 = isContainsSemi sb1  in
            let b4 = isContainsSemi sb2  in
-           (* print_string (sb1 ^ "  1\n"); *)
-           (* print_string (sb2 ^ "   1\n"); *)
            match(b3, b4)  with
                (true ,true ) ->  "(" ^ "("^sb1^")" ^  " + "  ^"("^ sb2 ^")" ^ ")"
              | (true, false)->   "(" ^ "("^sb1^")" ^  " + " ^ sb2 ^ ")"
@@ -89,59 +112,62 @@ and fixStmt (s : stmt) : string =
              | (false, false) ->  "(" ^sb1 ^  " + "  ^ sb2  ^ ")"
          )
      | (true, false) ->
-       (
+       ( let nstr= ( if (String.length sb2) >0 then sb2 else "0" ) in
          let b3 = isContainsSemi sb1  in
          (* print_string (sb1 ^"   " ^string_of_bool b3 ^ "   2\n"); *)
-         if b3 then   "(" ^ "("^sb1^")" ^  " + " ^ "0" ^ ")" else
-           "(" ^sb1 ^  " + "  ^ "0"  ^ ")"
+         if b3 then   "(" ^ "("^sb1^")" ^  " + " ^ (deleteSemi nstr) ^ ")" else
+           "(" ^sb1 ^  " + "  ^ (deleteSemi nstr)  ^ ")"
        )
      | (false, true) ->
-       (
+       ( let nstr =(if (String.length sb1) > 0 then sb1 else "0" ) in
          let b3 = isContainsSemi sb2  in
-         (* print_string (sb1 ^ "    3\n"); *)
-         if b3 then   "("^"0" ^  " + "  ^"("^ sb2 ^")" ^ ")"
-         else "(" ^"0" ^  " + "  ^ sb2  ^ ")"
+         if b3 then   "("^ (deleteSemi nstr) ^  " + "  ^"("^ sb2 ^")" ^ ")"
+         else "(" ^ (deleteSemi nstr) ^  " + "  ^ sb2  ^ ")"
        )
-     | (false, false) ->  "(" ^ "0" ^  " + "  ^ "0"  ^ ")"
+     | (false, false) ->
+        let nstr1 = (if (String.length sb1)>0 then sb1 else "0") in
+        let nstr2 = (if (String.length sb2)>0 then sb2 else "0") in
+        "(" ^ (deleteSemi nstr1) ^  " + "  ^ (deleteSemi nstr2)  ^ ")"
      end
   | Switch(_,b,_,_) ->
-     let sb = fixBlock b in
+     let sb = fixBlock fname b in
      let b= findMorF sb in
      begin
        match b with
-         true ->   " (" ^ sb ^ ") "
+         true ->   "(" ^ sb ^ ")"
        | false -> "0"
      end
   | Loop(b,_,_,_) ->
-     let sb = fixBlock b in
+     let sb = fixBlock fname b in
      let b = findMorF sb in
      begin
        match b with
-           true ->  "(ua. " ^sb ^ "a)"
-       | false -> "(ua. " ^ "0" ^ ";a)"
+           true ->  "(ua." ^sb ^ "a)"
+       | false -> "(ua." ^ "0" ^ ";a)"
      end
   | Block b ->
-     fixBlock b
+     fixBlock fname b
   | TryFinally(b1, b2, _) ->
-    "(" ^ (fixBlock b1) ^ " f+ " ^ (fixBlock b2 )^ " ) "
+    "(" ^ (fixBlock fname b1) ^ " f+ " ^ (fixBlock fname b2 )^ ")"
   | TryExcept(b1,_,b2,_) ->
-     " ( " ^  (fixBlock b1 ) ^  " try+ "  ^ (fixBlock b2 )  ^ " ) "
-  | Return _ -> ""
+     "(" ^  (fixBlock fname b1 ) ^  " try+ "  ^ (fixBlock fname b2 )  ^ ")"
+  | Return (None, loc) -> ""
+  | Return (Some expr, loc) -> fixExpr fname expr
   | Goto _ -> ""
   | Break _ -> ""
   | Continue _ -> ""
   | _ -> print_string " stms unknown "; ""
 
-and fixstmts (stmts: stmt list) : string =
+and fixstmts (fname) (stmts: stmt list) : string =
   match stmts with
       [] -> ""
-    | s:: [] ->  (fixStmt s)
-    | s :: rest ->let str = ( fixStmt s) in
-                  if (findMorF str) then str ^";" ^ (fixstmts rest)  else (fixstmts rest)
+    | s:: [] ->  (fixStmt fname s)
+    | s :: rest ->let str = ( fixStmt fname s) in
+                  if (String.length str) > 0 then ( deleteSemi str) ^ ";"^ (fixstmts fname rest)  else (fixstmts fname rest)
 
-and fixBlock (b : block)  = fixstmts b.bstmts
+and fixBlock (fname : string) (b : block)  = fixstmts fname b.bstmts
 
-and fixFunction (fd : fundec)  = fixBlock fd.sbody
+and fixFunction (fd : fundec)  = fixBlock (fd.svar.vname) fd.sbody
 
 and printfuns (flists: (string*string)list) : unit  =
   match flists with
@@ -160,7 +186,17 @@ let abstract (f : file) : unit =
                   | GFun (fd, loc)  ->
                      if (fd.svar.vname = "main") then
                        ()
-                     else funclist :=  (fd.svar.vname, fixFunction fd) :: !funclist
+                     else (
+                       let nstr = fixFunction fd in
+                       if (String.contains nstr 'a') then
+                         (
+                           funclist :=  (fd.svar.vname, "(ua." ^ "(" ^nstr^")" ^")") :: !funclist
+                         )
+                       else
+                         (
+                           funclist :=  (fd.svar.vname, nstr) :: !funclist
+                         )
+                     )
                   | _->() )
                  f.globals );
 
@@ -168,7 +204,7 @@ let abstract (f : file) : unit =
                   match g with
                   | GFun (fd, loc) ->
                      if (fd.svar.vname = "main") then
-                        funclist :=  (fd.svar.vname, fixFunction fd) :: !funclist
+                       funclist :=  (fd.svar.vname, fixFunction fd) :: !funclist
                      else ()
                   | _ -> () )
                  f.globals );
@@ -177,5 +213,5 @@ let abstract (f : file) : unit =
     printfuns funclist.contents;
     print_newline ();
 
-      with
+  with
     _ -> print_string "Error: abs.ml"; print_newline ()
--- t2.c
+++ t2.c
@@ -10,13 +10,55 @@ struct Connection{
   int year;
 };
 
+int fun (int n){
 
+  if (n<=1)
+    {
+      return 1;
+    }
+  else {
+    return n + fun(n-1);
+  }
+}
+
+int func()
+{
+
+  int **q;
+  int n = 1 + 5;
+
+ struct Connection *conn = malloc(sizeof(struct Connection));
+
+ int *p = (int*)malloc(sizeof(int));
+
+ int m = 1;
+ func();
+ int c ;
+ scanf("%d",&c);
+
+ conn->db =malloc(sizeof(struct Database));
+
+  if(conn)
+    {
+      if(conn->db) free(conn->db);
+
+      m = m+ 2;
+
+      free(conn);
+
+    }
+
+
+  return 0;
+}
 
 int main()
 {
 
   int **q;
   int n = 1 + 5;
+
+  printf("%d\n", fun(n));
 
  struct Connection *conn = malloc(sizeof(struct Connection));
 
@@ -37,7 +79,6 @@ int main()
 
       free(conn);
 
-      free(conn->db->addre);
     }
 
 
